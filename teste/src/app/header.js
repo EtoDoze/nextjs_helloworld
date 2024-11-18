fc8e3911b80309f9011d2a27362bba358f0ae0d5
Eto12 <reverso12yt@gmail.com>
2024-11-17 23:32:42 -0300

    a

---
+++ teste/src/app/header.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function HeaderPage() {
+    return (
+    <header>
+        <h4>Site</h4>
+        <p>Inicio</p>
+        <p>pag2</p>
+        <p>pag3</p>
+        <p>pag4</p>
+    </header>
+    );
+  }
--- teste/src/app/page.js
+++ teste/src/app/page.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeaderPage from "./header";
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <>
+    <HeaderPage/>
+    <div className="box-cont">
+    <div className="box">
     <h1>Hello World</h1>
     <p>Bem vindo a pagina inicial</p>
+    </div>
+    </div>
     </>
   );
 }
